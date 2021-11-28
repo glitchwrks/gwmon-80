70b20961b4a7b547241a60927441aee568077367
Jonathan Chapman <chapmajs@gmail.com>
2021-11-28 18:10:42 -0500

    WIP XM 20211128

---
+++ xmmits1.asm
@@ -0,0 +1,23 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;XMMITS1 -- GWMON-80 Extended Monitor for MITS 88-2SIO
+;
+;This customization uses channel 0 on a MITS 88-2SIO with
+;default strapping for the console.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;Hardware Equates
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+CTLPRT	equ	010H		;88-2SIO channel 0 control port
+DATPRT	equ	011H		;88-2SIO channel 0 data port
+TOPMEM	equ	0C000H		;48K system
+
+	ORG	0F800H
+
+	INCLUDE	'xm.inc'
+	INCLUDE '6850acia.inc'
+
+	INCLUDE 'xcmdstd.inc'	;Standard command list
+	INCLUDE 'xcmdnull.inc'	;NULL command, closes command list
+
+	END
